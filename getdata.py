8b8b4b34ccec13f0f9010e589324b64af4196ddd
everywherewego <964353527@qq.com>
2019-11-17 15:54:59 +0800

    优化

--- getdata.py
+++ getdata.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
 import time
-from threading import Timer
-
 import requests
 import json
 import sys
-
-reload(sys)
-sys.setdefaultencoding('utf8')
-
 from mail import mail
 
-oldstatus = ["4432072237827758",
-             "4432047051360708",
-             "4431734302623863",
-             "4431733091279116",
-             "4431060550860686",
-             "4430993596876596",
-             "4430682749117386",
-             "4428750211387049",
-             "4428439836730962",
-             "4428375302848802"]
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def read_file(filepath):
+    with open(filepath) as fp:
+        content = fp.read()
+    return content
+
+
+def append_file(filepath, content):
+    with open(filepath, 'a') as af:
+        af.write(content + '\n')
 
 
 def gethttp():
@@ -30,42 +27,43 @@ def gethttp():
 
     # byte转str
     result = response.content.decode('utf-8')
-
     # str转json
     all_news = json.loads(result)
-    # print(all_news)
-
     infos = all_news["data"]["statuses"]
-    # print(infos)
     return infos
 
 
 def get_news():
     infosaaa = gethttp()
+    oldstatus = read_file("code.txt")
     # 遍历由json数据得到的list
     for info in infosaaa:
-        text = info["text"]
+        idnum = info["id"]
+        if idnum not in oldstatus:
+            # 动态内容
+            text = info["text"]
 
-        picurl = []
-        if "pics" in info.keys():
-            for pic in info["pics"]:
-                # print(pic["large"]["url"])
-                picurl.append(pic["large"]["url"])
+            # 动态图片
+            picurl = []
+            if "pics" in info.keys():
+                for pic in info["pics"]:
+                    picurl.append(pic["large"]["url"])
 
-        retweeted = []
-        if "retweeted_status" in info.keys():
-            print (info["retweeted_status"]["text"])
-            retweeted.append(info["retweeted_status"]["text"])
+            # 转发内容
+            retweeted = []
+            if "retweeted_status" in info.keys():
+                print (info["retweeted_status"]["text"])
+                retweeted.append(info["retweeted_status"]["text"])
 
-        id = info["id"]
-        if id not in oldstatus:
             print(text + "\n" + str(picurl) + "\n" + str(retweeted))
-            oldstatus.append(id)
+            append_file('code.txt', idnum)
             mail(text + "\n" + str(picurl) + "\n" + str(retweeted))
-    print oldstatus
 
 
 if __name__ == "__main__":
     while True:
-        get_news()
+        try:
+            get_news()
+        except BaseException as e:
+            print(e.message)
         time.sleep(10 * 60)
